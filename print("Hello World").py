cdb96f521d0893703318408e28ab46e6e9b599f6
mayayl23-meet <109899357+mayayl23-meet@users.noreply.github.com>
2022-07-26 09:32:42 +0300

    Create print("Hello World").py

---
+++ print("Hello World").py
@@ -0,0 +1,4 @@
+print("Hello World")
+print("My name is Maya Yedid-Levi")
+print("I am 16 years old and I live in Tel Aviv")
+print("A fun fact about me is that I lived abroad for 11 years and I was in competitive gymnastics for 5 years")
